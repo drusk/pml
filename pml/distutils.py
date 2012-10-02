87f69239f2793550c6af3af33970ac11c68589e1
drusk <drusk@uvic.ca>
2012-10-01 22:49:44 -0700

    Added distutils module with method for calculating Euclidean distances between two vectors.

---
+++ pml/distutils.py
@@ -0,0 +1,20 @@
+"""
+Algorithms for calculating distances between vectors in feature (n) space.
+
+@author: drusk
+"""
+
+import numpy as np
+
+def euclidean(vector1, vector2):
+    """
+    Calculates the Euclidean distance between two vectors in n-space.
+    
+    Args:
+      vector1: start point vector
+      vector2: end point vector
+    
+    Returns:
+      The distance (magnitude) between vector1 and vector2.
+    """
+    return np.sqrt(np.power(np.asarray(vector1) - np.asarray(vector2), 2).sum())
---
+++ test/distutils_tests.py
@@ -0,0 +1,36 @@
+"""
+Unit tests for the distutils module.
+
+@author: drusk
+"""
+
+import unittest
+import distutils
+
+class DistutilsTest(unittest.TestCase):
+
+    def setUp(self):
+        self.delta = 0.01
+
+    def testEuclidean2d(self):
+        vector1 = [9, 6]
+        vector2 = [5, 3]
+        distance = distutils.euclidean(vector1, vector2)
+        self.assertEqual(distance, 5) 
+
+    def testEuclidean2dNegative(self):
+        vector1 = [-12, 2]
+        vector2 = [6, -5]
+        distance = distutils.euclidean(vector1, vector2)
+        self.assertAlmostEqual(distance, 19.313, delta=self.delta)
+        
+    def testEuclidean3d(self):
+        vector1 = [-4, 9, 6]
+        vector2 = [8, -2, -7]
+        distance = distutils.euclidean(vector1, vector2)
+        self.assertAlmostEqual(distance, 20.833, delta=self.delta)
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
+    
