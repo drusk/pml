0032a255113cfa0f221b71449d1d2e7a4082ed35
drusk <drusk@uvic.ca>
2012-10-06 14:33:17 -0700

    Refactoring

--- pml/classifiers.py
+++ pml/classifiers.py
@@ -7,7 +7,6 @@ Classification algorithms for supervised learning tasks.
 import loader
 import distance_utils
 import collections
-import collection_utils
 
 class Knn(object):
     """
@@ -59,12 +58,50 @@ class Knn(object):
         
         distances = data.reduce_rows(calc_dist)
         
+        votes = self._tally_votes(labels, distances)
+        most_voted = self._get_most_voted(votes)
+        
+        # TODO tie breaking?
+        assert len(most_voted) >= 1
+        return most_voted[0]
+
+    def _tally_votes(self, labels, distances):
+        """
+        Counts the k nearest neighbours' votes for which classification to 
+        give the sample.
+        
+        Args:
+          labels: the training set labels
+          distances: the distance from each entry in the training set to the 
+            sample.
+              
+        Returns: 
+          a dictionary mapping labels to their number of votes.
+        """
         votes = collections.defaultdict(int)
         for i, index in enumerate(distances.order(ascending=True).index):
             if i < self.k:
                 votes[labels[index]] += 1
             else:
                 break
-        
-        return collection_utils.get_key_of_highest_value(votes)
+        return votes
     
+    def _get_most_voted(self, votes):
+        """
+        Determines the labels which received the most votes.
+        
+        Args:
+          votes: a dictionary mapping labels to their number of votes.
+          
+        Returns:
+          a list of the labels which got the most votes.  It is a list because 
+          there may be ties.
+        """
+        max_votes = max(votes.values())
+        most_voted = []
+        
+        for label, num_votes in votes.iteritems:
+            if num_votes == max_votes:
+                most_voted.append(label)
+        
+        return most_voted
