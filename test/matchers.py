e5531d3931c48349ada0b4d4512d8d6de51ba3fc
drusk <drusk@uvic.ca>
2012-10-13 14:29:36 -0700

    #11: Added DataSet matcher.

---
+++ test/matchers.py
@@ -0,0 +1,79 @@
+# Copyright (C) 2012 David Rusk
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy 
+# of this software and associated documentation files (the "Software"), to 
+# deal in the Software without restriction, including without limitation the 
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+# sell copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in 
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+# IN THE SOFTWARE.
+"""
+Custom Hamcrest matchers.
+
+@author: drusk
+"""
+
+from hamcrest.core.base_matcher import BaseMatcher
+
+class IsDataSet(BaseMatcher):
+    """
+    Used for asserting the contents of a DataSet object are as expected.
+    """
+    
+    def __init__(self, as_list):
+        """
+        Creates a new matcher given an expected input.
+        
+        Args:
+          as_list: 2d list
+            A 2d list where each sub-list represents a row in the dataset's 
+            underlying DataFrame.
+        """
+        self.as_list = as_list
+    
+    def _matches(self, dataset):
+        print "Expected: %s" % self.as_list
+        if dataset.num_samples() != len(self.as_list):
+            return False
+        print "Length check ok"
+        for i in xrange(dataset.num_samples()):
+            print "Checking element %d" %i
+            print self.as_list[i]
+            print "Dataset: %s" % dataset
+            print dataset.get_row(i)
+            print dataset.get_row(i).tolist()
+            if not match_exactly(self.as_list[i], dataset.get_row(i).tolist()):
+                return False
+        
+        return True    
+        
+    def describe_to(self, description):
+        description.append_text("dataset with elements: ")
+        description.append_text(self.as_list)
+    
+def match_exactly(list1, list2):
+    """
+    Compares two lists and returns True if they are exactly the same, False 
+    otherwise.
+    """
+    print "Testing match_exactly"
+    print "list1: " % list1
+    print "list2: " % list2
+    return len(list1) == len(list2) and \
+        all([list1[i] == list2[i] for i in xrange(len(list1))])
+    
+def equals_dataset(as_list):
+    """
+    Compares a DataSet object to the given list representation.
+    """
+    return IsDataSet(as_list)
