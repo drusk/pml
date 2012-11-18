3150c5c7c35fbb9effe6a3e9a3243920fba25be7
drusk <drusk@uvic.ca>
2012-11-17 20:42:35 -0800

    #45: mocked out matplotlib for docs build.

--- docs/conf.py
+++ docs/conf.py
@@ -304,6 +304,6 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['pandas']
+MOCK_MODULES = ['pandas', 'matplotlib', 'matplotlib.pyplot']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
