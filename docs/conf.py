558c3ccea45459160aa1af5e548cbaaca731b282
drusk <drusk@uvic.ca>
2012-11-17 20:45:52 -0800

    #45: mocked out pandas.tools.plotting and numpy for docs build.

--- docs/conf.py
+++ docs/conf.py
@@ -304,6 +304,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['pandas', 'matplotlib', 'matplotlib.pyplot']
+MOCK_MODULES = ['pandas', 'pandas.tools.plotting', 'matplotlib', 
+                'matplotlib.pyplot', 'numpy']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
