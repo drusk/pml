bb8418afecc7ce1ab96ec2561b7cac77e485cfbe
drusk <drusk@uvic.ca>
2012-11-18 21:48:02 -0800

    #1: added naive_bayes module and test module with testing data set.  Added method for counting training examples with the specified class and feature value.

---
+++ pml/supervised/naive_bayes.py
@@ -0,0 +1,73 @@
+# Copyright (C) 2012 David Rusk
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy 
+# of this software and associated documentation files (the "Software"), to 
+# deal in the Software without restriction, including without limitation the 
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+# sell copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in 
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+# IN THE SOFTWARE.
+"""
+Naive Bayes classification algorithm.
+
+@author: drusk
+"""
+
+class NaiveBayes(object):
+    """
+    """
+    
+    def __init__(self, training_set):
+        """
+        Constructs a new NaiveBayes classifier.
+        
+        Args:
+          training_set: model.DataSet
+            The data used to train the classifier.
+        """
+        self._training_set = training_set
+    
+    def _count_examples_with_class_and_feature_val(self, clazz, feature, sample):
+        """
+        Counts the training set examples which have the specified class as 
+        well as the same value for the specified feature as the provided 
+        sample.
+        
+        Args:
+          clazz:
+            The class which training examples must belong to in order to be 
+            counted.
+          feature:
+            The feature for which the training examples must have the same 
+            value as the sample.
+          sample: dict or dict-like (ex: pandas.Series)
+            The sample whose value for the specified feature must be matched 
+            in order to count an example.
+            
+        Returns:
+          count: int
+            The number of training examples with the specified class and same 
+            value as the sample for the specified feature.
+        """
+        training_classes = self._training_set.get_labels()
+        training_feature_vals = self._training_set.get_column(feature)
+        
+        count = 0
+        for sample_id in self._training_set.get_sample_ids():
+            if (training_classes[sample_id] == clazz and 
+                training_feature_vals[sample_id] == sample[feature]):
+                count += 1
+                
+        return count
+    
+    
---
+++ test/naive_bayes_tests.py
@@ -0,0 +1,60 @@
+# Copyright (C) 2012 David Rusk
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy 
+# of this software and associated documentation files (the "Software"), to 
+# deal in the Software without restriction, including without limitation the 
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+# sell copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in 
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+# IN THE SOFTWARE.
+"""
+Unit tests for naive_bayes module.
+
+@author: drusk
+"""
+
+import unittest
+
+from pml.data import loader
+from pml.supervised.naive_bayes import NaiveBayes
+
+import base_tests
+
+class NaiveBayesTest(base_tests.BaseFileLoadingTest):
+
+    def load_car_data(self):
+        """
+        Loads an example training set and related sample.
+         
+        NOTE: pandas automatically converts columns with just 'yes' and 'no' 
+        values into booleans.
+        """
+        training_set = loader.load(self.relative("datasets/car_thefts.data"))
+        sample = {
+                  "color": "red",
+                  "type": "suv",
+                  "origin": "domestic"
+                  }
+        return training_set, sample
+
+    def test_count_examples_with_class_and_feature_val(self):
+        training_set, sample = self.load_car_data()
+        classifier = NaiveBayes(training_set)
+        count = classifier._count_examples_with_class_and_feature_val(True, "color", sample)
+        self.assertEquals(count, 3)
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
+    
