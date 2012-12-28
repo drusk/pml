5e9aa1e2d8a9eb3a82a030b495c2237acb2dd30a
drusk <drusk@uvic.ca>
2012-12-27 18:07:13 -0800

    #48: docs for tree data structures.

--- pml/supervised/trees.py
+++ pml/supervised/trees.py
@@ -18,58 +18,119 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
 # IN THE SOFTWARE.
 """
-TODO: Short module level description.
+Data structures for representing trees.
 
 @author: drusk
 """
 
 class Tree(object):
     """
+    A tree containing nodes which are connected to each other by directed 
+    edges.
     """
     
     def __init__(self, root_node):
         """
+        Constructs a new tree.
+        
+        Args:
+          root_node: Node
+            The node which will be at the root of the tree.
         """
         self._root_node = root_node
         
     def get_root_node(self):
         """
+        Retrieves the root node of the tree.  This node will have only 
+        outgoing edges (children), no incoming edges (parents).
+        
+        Returns:
+          root: Node
+            The root node.
         """
         return self._root_node
     
 
 class Node(object):
     """
+    A node in a tree.  Holds a value and may have branches connecting it to
+    other nodes.
     """
     
     def __init__(self, value):
         """
+        Constructs a new node.
+        
+        Args:
+          value:
+            The data value to be associated with this node.
         """
         self._value = value
         self._children = {}
     
     def get_value(self):
         """
+        Retrieves the data value associated with this node.
+        
+        Returns:
+          value:
+            The value associated with this node.
         """
         return self._value
     
     def add_child(self, branch, child):
         """
+        Creates a branch from this node to another node which will be the 
+        child.
+        
+        Args:
+          branch:
+            The identifier for the branch connecting this node to the 
+            child node.
+          child: Node
+            Another node which will be a child of the current node.
+        
+        Returns:
+          void
         """
         self._children[branch] = child
     
     def get_child(self, branch):
         """
+        Retrieves the child node connected by the specified branch.
+        
+        Args:
+          branch:
+            The identifier that was used to associate a child node with
+            the current node.
+            
+        Returns:
+          child: Node
+            The child node found by following the specified branch.
+            
+        Raises:
+          KeyError if the specified branch does not exist.
         """
         return self._children[branch]
     
     def get_branches(self):
         """
+        Retrieves all the branches to children of the current node.
+        
+        Returns:
+          branches: list
+            A list of all the branches to child nodes.  Note that this means
+            branches TO this node are not included.
         """
         return self._children.keys()
     
     def is_leaf(self):
         """
+        Checks if this node is a leaf (has no children).
+        
+        Returns:
+          is_leaf: boolean
+            True if this node has no children.
         """
         return len(self._children) == 0
     
